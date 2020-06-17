3f7b355341eb60596e39e1dd20bee1966ab5ecc1
TakahashiKazuteru <kazuteru.takahashi@rakus.co.jp>
2020-06-17 15:35:23 +0900

    社員情報を操作するときに使用するフォームの追加

---
+++ src/main/java/jp/co/sample/service/EmployeeService.java
@@ -0,0 +1,33 @@
+package jp.co.sample.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.repository.EmployeeRepository;
+
+/**
+ * 社員情報を操作するサービス.
+ * 
+ * @author kazuteru.takahashi
+ *
+ */
+@Service
+@Transactional
+public class EmployeeService {
+	@Autowired
+	private EmployeeRepository repository;
+	
+	/**
+	 * 社員情報全件検索.
+	 * 
+	 * @return 社員情報全件(入社日降順)
+	 */
+	public List<Employee> showList(){
+		return repository.findAll();
+	}
+
+}
