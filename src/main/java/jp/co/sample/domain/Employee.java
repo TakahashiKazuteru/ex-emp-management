43ebe4bd0ae029914dbdd0b6a83fb4f4ad378f86
TakahashiKazuteru <kazuteru.takahashi@rakus.co.jp>
2020-06-17 15:28:13 +0900

    コンストラクタの追加

--- src/main/java/jp/co/sample/domain/Employee.java
+++ src/main/java/jp/co/sample/domain/Employee.java
@@ -34,6 +34,26 @@ public class Employee {
 	/** 扶養人数 */
 	private Integer dependentsCount;
 
+	public Employee() {
+	}
+
+	public Employee(Integer id, String name, String image, String gender, Date hireDate, String mailAddress,
+			String zipCode, String address, String telephone, Integer salary, String characteristics,
+			Integer dependentsCount) {
+		this.id = id;
+		this.name = name;
+		this.image = image;
+		this.gender = gender;
+		this.hireDate = hireDate;
+		this.mailAddress = mailAddress;
+		this.zipCode = zipCode;
+		this.address = address;
+		this.telephone = telephone;
+		this.salary = salary;
+		this.characteristics = characteristics;
+		this.dependentsCount = dependentsCount;
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", image=" + image + ", gender=" + gender + ", hireDate="
