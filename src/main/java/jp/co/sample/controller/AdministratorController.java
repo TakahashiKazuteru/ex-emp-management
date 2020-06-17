c9eceaecfb0f1836b425ee7872cbaa0ff4ee61e5
TakahashiKazuteru <kazuteru.takahashi@rakus.co.jp>
2020-06-17 15:56:29 +0900

    社員リストへの画面遷移を修正

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -68,7 +68,7 @@ public class AdministratorController {
 			return toLogin(model);
 		}
 		session.setAttribute("administratorName", administrator.getName());
-		return "foward:/employee/showList";
+		return "forward:/employee/showList";
 	}
 
 	/**
