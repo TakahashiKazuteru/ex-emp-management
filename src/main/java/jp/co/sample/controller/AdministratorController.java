dba25a9636851a74c653a918ed9eecc978bde925
TakahashiKazuteru <kazuteru.takahashi@rakus.co.jp>
2020-06-17 15:18:24 +0900

    ログイン処理の追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 /**
@@ -22,38 +26,71 @@ import jp.co.sample.service.AdministratorService;
 @RequestMapping("/")
 public class AdministratorController {
 	@Autowired
-	private AdministratorService service;
+	private HttpSession session;
 	
+	@Autowired
+	private AdministratorService service;
+
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
+
 	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
 		return new InsertAdministratorForm();
 	}
-	
+
+	/**
+	 * ログイン画面の表示
+	 * 
+	 * @param model リクエストスコープ
+	 * @return ログイン画面
+	 */
 	@RequestMapping("/")
-	public String toLogin() {
+	public String toLogin(Model model) {
 		return "administrator/login";
 	}
-	
+
+	/**
+	 * ログイン処理を行う
+	 * 
+	 * @param form ログイン情報
+	 * @param model リクエストスコープ
+	 * @return ログイン成功ならば従業員一覧へ(失敗ならばログイン画面へ戻る)
+	 */
+	@RequestMapping("/login")
+	public String login(LoginForm form,Model model) {
+		
+		Administrator administrator = service.findByMailaddressAndPassword(form.getMailAddress(), form.getPassword());
+		if (administrator == null) {
+			model.addAttribute("message", "メールアドレスまたはパスワードが不正です");
+			return toLogin(model);
+		}
+		session.setAttribute("administratorName", administrator.getName());
+		return "foward:/employee/showList";
+	}
+
 	/**
 	 * ログイン画面をレスポンスする.
 	 * 
 	 * @return ログイン画面
 	 */
 	@RequestMapping("/toInsert")
-	public String toInsert() { 
+	public String toInsert() {
 		return "administrator/insert";
 	}
-	
+
 	/**
 	 * 入力された情報を登録する.
 	 * 
 	 * @param form 入力情報
-	 * @param br エラー情報
+	 * @param br   エラー情報
 	 * @return ホーム画面(リダイレクト)
 	 */
 	@RequestMapping("/insert")
-	public String insert(@Validated InsertAdministratorForm form,BindingResult br) {
-		if(br.hasErrors()) {
+	public String insert(@Validated InsertAdministratorForm form, BindingResult br) {
+		if (br.hasErrors()) {
 			return toInsert();
 		}
 		Administrator administrator = new Administrator();
